670d09979a9d7783f9deb5b136048d6d8bf42394
etherio97 <ethereal97@gmail.com>
2022-04-22 20:08:47 +0630

    bug fixed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import { readFile, writeFile, readJson } from 'fs-extra';
-import { getTiktokTopHits } from './internals';
+import { getTiktokTopHits } from './internals/getTiktokTopHits';
 
 const ROOT_DIR = process.cwd();
 const STUB_DIR = resolve(ROOT_DIR, 'stubs');
